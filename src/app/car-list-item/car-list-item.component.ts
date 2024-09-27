a2a2003d3b494e03afb98a73caa8beb6514009dc
komalHitang <w0855062@myscc.ca>
2024-09-27 19:18:56 -0400

    making some change in all

--- src/app/car-list-item/car-list-item.component.ts
+++ src/app/car-list-item/car-list-item.component.ts
@@ -1,21 +1,15 @@
-import {Component, Input} from '@angular/core';
-
-import {NgIf} from "@angular/common";
-import {Car} from "../car/car";
+import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Car } from '../car/car';
 
 @Component({
   selector: 'app-car-list-item',
   standalone: true,
-  imports: [
-    NgIf
-  ],
+  imports: [NgIf],
   templateUrl: './car-list-item.component.html',
-  styleUrl: './car-list-item.component.css'
+  styleUrls: ['./car-list-item.component.css']
 })
 export class CarListItemComponent {
-  @Input() choosedcar? :Car;
-  car: any;
-  Car: any;
-
+  @Input() car!: Car; // Use this to accept car data
 
 }
--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,76 +1,50 @@
-import {Component, Input} from '@angular/core';
-import {NgForOf} from "@angular/common";
-import {Car} from "../car/car";
-// @ts-ignore
-
-
-
-class car {
-}
+import { Component } from '@angular/core';
+import { Car } from '../car/car';
 
 @Component({
-  selector: 'app-car-list',
-  standalone: true,
-  imports: [
-    NgForOf,
-
-
-  ],
-  templateUrl: './car-list.component.html',
-  styleUrl: './car-list.component.css'
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.css']
 })
-export class CarListComponent {
-  displayedColumns:string[]= ['type', 'model', 'vinNumber', 'color', 'brand','hasOwner','image'];
-    carList: Car[] = [
-      {
-        type:'toyota',
-        model:'camry',
-        vinNumber:4545,
-        color:'black',
-        brand:'Toyota',
-        hasOwner: false,
-        image:'download.jpg'
-
-
+export class ContentListComponent {
+  chosenContent = 'car';
+  carList: Car[] = [
+    {
+      type: 'toyota',
+      model: 'camry',
+      vinNumber: 4545,
+      color: 'black',
+      brand: 'Toyota',
+      hasOwner: false,
+      image: 'assets/images/download.jpg'
     },
-      {
-        type: 'ford',
-        model: 'mustang',
-        vinNumber: 2709,
-        color:'blue',
-        brand:'Ford',
-        hasOwner:true,
-        image:'mustang.jpg'
-
-      },
-      {
-        type:'tesla',
-        model:'model X',
-        vinNumber:6757,
-        color:'black',
-        brand:'Tesla',
-        hasOwner:false,
-        image:'tesla.jpg'
-
-      },
-      {
-        type:'honda',
-        model:'civic',
-        vinNumber: 5614,
-        color:'black',
-        brand:'Honda',
-        hasOwner:true,
-        image:'civic.jpg'
-
-      }];
-  //Catch the onclick event from the html
-  selectedcar?: car;
-  //function to set which car to display
-  @Input() Car!: any;
-  choosedcar: any;
-  selectcar(choosedcar:car): void {
-    this.selectedcar = choosedcar;
-  }
-
- }
+    {
+      type: 'ford',
+      model: 'mustang',
+      vinNumber: 2709,
+      color: 'blue',
+      brand: 'Ford',
+      hasOwner: true,
+      image: 'assets/images/mustang.jpg'
+    },
+    {
+      type: 'tesla',
+      model: 'model X',
+      vinNumber: 6757,
+      color: 'black',
+      brand: 'Tesla',
+      hasOwner: false,
+      image: 'assets/images/tesla.jpg'
+    },
+    {
+      type: 'honda',
+      model: 'civic',
+      vinNumber: 5614,
+      color: 'black',
+      brand: 'Honda',
+      hasOwner: true,
+      image: 'assets/images/civic.jpg'
+    }
+  ];
+}
 
