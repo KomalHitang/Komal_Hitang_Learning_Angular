9df788b7a35b0c293bee6560e60e9a197f423807
W0855062 <w0855062@myscc.ca>
2024-10-11 13:16:21 -0400

    modifying the car list component

--- src/app/car-list-item/car-list-item.component.ts
+++ src/app/car-list-item/car-list-item.component.ts
@@ -1,15 +1,64 @@
-import { Component, Input } from '@angular/core';
-import { NgIf } from '@angular/common';
-import { Car } from '../car/car';
+import {Component, Input, OnInit} from '@angular/core';
+import {car} from "../car/car";
+import {NgIf} from "@angular/common";
+import {ActivatedRoute, Router} from "@angular/router";
+import {carService} from "../Services/car.service";
 
 @Component({
-  selector: 'app-car-list-item',
+  selector: 'app-car-detail',
   standalone: true,
-  imports: [NgIf],
-  templateUrl: './car-list-item.component.html',
-  styleUrls: ['./car-list-item.component.css']
+  imports: [
+    NgIf
+  ],
+  templateUrl: './car-detail.component.html',
+  styleUrl: './car-detail.component.scss'
 })
-export class CarListItemComponent {
-  @Input() car!: Car; // Use this to accept car data
+export class carDetailComponent implements OnInit{
+  //Needs to be | undef because there wont always be a car thats clicked on
+  car: car | undefined; //The car to display
+  mockContent: car[] = [];// to store the list of car
+  currentIndex: number = 0;//to track the current index
+
+  constructor(
+    private route: ActivatedRoute,
+    private carService: carService,
+    private router: Router
+  ) {}
+//rewrite onInit to get the list of car and the current student
+  ngOnInit(): void {
+    this.carService.getStudents().subscribe(car => {
+      this.mockContent = car;
+
+      // Subscribe to paramMap changes to actually see the page changing
+      //If we dont do this, the URL will change but the view will not
+      this.route.paramMap.subscribe(params => {
+        const id = Number(params.get('id'));
+        if (id) {
+          this.currentIndex = this.mockContent.findIndex(user => car.id === id);
+          this.car = this.mockContent[this.currentIndex];
+        }
+      });
+    });
+  }
+
+//function to go back to car-list view
+  goBack(): void {
+    this.router.navigate(['/car']);
+  }
+
+  goForward(): void {
+    if (this.currentIndex < this.mockContent.length - 1) {
+      this.currentIndex++;
+      this.router.navigate(['/car', this.mockContent[this.currentIndex].id]);
+    }
+  }
+
+  goBackward(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.router.navigate(['/car', this.mockContent[this.currentIndex].id]);
+    }
+  }
+
 
 }
--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,57 +1,48 @@
-import { Component } from '@angular/core';
-import { Car } from '../car/car';
-import {CarListItemComponent} from "../car-list-item/car-list-item.component";
+import {Component, OnInit} from '@angular/core';
+// @ts-ignore
+import { User} from "../Shared/Models/user";
 import {NgForOf} from "@angular/common";
-
+// @ts-ignore
+import {carDetailComponent} from "../car-detail/car-detail.component";
+// @ts-ignore
+import {carService} from "../Services/car.service";
+import {RouterLink} from "@angular/router";
 @Component({
   selector: 'app-car-list',
-  templateUrl: './car-list.component.html',
   standalone: true,
   imports: [
-    CarListItemComponent,
-    NgForOf
+    NgForOf,
+    carDetailComponent,
+    RouterLink
   ],
-  styleUrls: ['./car-list.component.css']
+  templateUrl: './car-list.component.html',
+  styleUrl: './car-list.component.scss'
 })
-export class CarListComponent {
-  chosenContent = 'car';
-  carList: Car[] = [
-    {
-      type: 'toyota',
-      model: 'camry',
-      vinNumber: 4545,
-      color: 'black',
-      brand: 'Toyota',
-      hasOwner: false,
-      image: 'download.jpg'
-    },
-    {
-      type: 'ford',
-      model: 'mustang',
-      vinNumber: 2709,
-      color: 'blue',
-      brand: 'Ford',
-      hasOwner: true,
-      image: 'mustang.jpg'
-    },
-    {
-      type: 'tesla',
-      model: 'model X',
-      vinNumber: 6757,
-      color: 'black',
-      brand: 'Tesla',
-      hasOwner: false,
-      image: 'tesla.jpg'
-    },
-    {
-      type: 'honda',
-      model: 'civic',
-      vinNumber: 5614,
-      color: 'black',
-      brand: 'Honda',
-      hasOwner: true,
-      image: 'civic.jpg'
-    }
-  ];
+export class StudentListComponent implements OnInit {
+  //Placeholder values for the table
+  chosenContent:string[]= ['id', 'name', 'year', 'model'];
+  mockContent: User[] = [];
+
+  constructor (private carService: carService){
+    //this constructor is primarily used for dependency injection
+  }
+
+
+  ngOnInit(){
+    //This lifecycle hook is a good place to fetch and init our data
+    this.carService.getStudents().subscribe({
+      next: (data: User[]) => this.mockContent = data,
+      error: (err: any) => console.error("Error fetching car", err),
+      complete:() => console.log("Student data fetch complete!")
+    })
+
+  }
+  selectedcar?: User;
+  selectcar(car: User): void {
+    this.selectedcar = car;
+  }
+
 }
 
+export class carListComponent {
+}
