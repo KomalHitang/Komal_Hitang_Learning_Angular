7f5ea5491f6dcdee60cb34d2174eff741b19021d
w0855062 <w0855062@myscc.ca>
2024-11-11 09:11:36 -0500

    create a branch and create a new file and change version from 4 to 6

--- src/app/Services/car.service.ts
+++ src/app/Services/car.service.ts
@@ -1,45 +1,51 @@
 import { Injectable } from '@angular/core';
 //import our mock data
+import {userList} from "../Shared/mockStudent.data";
+import {catchError, Observable,  throwError} from 'rxjs';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {mockContent} from "../car/mockcar.data";
-import { Observable, of } from 'rxjs';
 import {Car} from "../car/car";
 
 //Notice the new Decorator
 @Injectable({
   providedIn: 'root'
 })
-export class carService {
+export class StudentService {
+  private apiUrl = 'api/students'; //url to web api
   private car: Car[] = mockContent;//Local copy of student data for CRUD Operations
-  constructor() { }
-  //Returns all students
-
-  getcar(): Observable< Car[]> {
-    return of(this.car);
+  constructor(private http: HttpClient) { }//DI http
+  //CRUD operations using HTTP Requests
+  //All operations we need are:
+  // Get, post, put, delete
+  getCar(): Observable<Car[]> {
+    return this.http.get<Car[]>(this.apiUrl).pipe(catchError(this.handleError));
   }
 
-  getcarById(id: number): Observable<Car | undefined> {
-    return of(this.car.find(car => car.id === id));
+  getCarById(id: number): Observable<Car> {
+    return this.http.get<Car>(`${this.apiUrl}/${id}`).pipe(catchError(this.handleError)); //return a single student
   }
 
-  addcar(car: Car): Observable<Car> {
-    this.car.push(car);
-    return of(car);
+  addCar(car: Car): Observable<Car> {
+    car.id = this.generateNewId();
+    return this.http.post<Car>(this.apiUrl, car).pipe(catchError(this.handleError));
   }
 
-  updatecar(updatedcar: Car): Observable<Car | undefined> {
-    const index = this.car.findIndex(car => car.id === updatedcar.id);
-    if (index > -1) {
-      this.car[index] = updatedcar;
-      return of(updatedcar);
-    }
-    return of(undefined);
+  updateCar(car: Car): Observable<Car | undefined> {
+    const url = `${this.apiUrl}/${car.id}`;
+    return this.http.put<Car>(url, car).pipe(catchError(this.handleError));
   }
 
-  deletecar(id: number): void {
-    this.car = this.car.filter(car => car.id !== id);
+  deleteCar(id: number): Observable<{}> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url).pipe(catchError(this.handleError));
   }
   // New method to generate a new unique ID
   generateNewId(): number {
     return this.car.length > 0 ? Math.max(...this.car.map(car => car.id)) + 1 : 1;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('API error:', error);
+    return throwError(() => new Error('Server error, please try again.'));
   }
 }
---
+++ src/app/Services/in-memory-data.service.ts
@@ -0,0 +1,24 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import {Car} from "../car/car";
+
+
+
+export class InMemoryDataService implements InMemoryDbService {
+  //returns an object with a students property,
+  // which is an array of User objects
+  createDb():{mockContent: Car[]} {
+    /*
+    Inside the method, a constant array named students is defined,
+    containing several User objects. Each User object represents a
+    student with properties such as id, firstName, lastName,
+     department, and isAdmin. For example, one of the User objects is
+     */
+    const mockContent: Car[] = [
+      { id: 1, name: 'Toyota Corolla', year: 2020, model: 'Sedan' },
+      { id: 2, name: 'Ford Mustang', year: 2021, model: 'Coupe' },
+      { id: 3, name: 'Tesla Model 3', year: 2022, model: 'Sedan' },
+
+    ];
+    return { mockContent };
+  }
+}
